1e876cadf7d654c16218d7379f2408898b70f08f
Miamasha <miliutina.mariia@gmail.com>
2023-12-16 23:52:38 +0200

    ефыл 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,3 +1,17 @@
+// Cкрипт, який змінює колір фону елемента <body> через
+// інлайн-стиль по кліку на button.change-color і задає
+// це значення кольору текстовим вмістом для span.color
+
+
+document.querySelector(".change-color").addEventListener(`click`, handleColor);
+
+function handleColor(event){
+  event.preventDefault();
+  const currentColor = getRandomHexColor();
+  document.querySelector("body").style.backgroundColor = currentColor;
+  document.querySelector("span").innerHTML = currentColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
